a45b3cdfac93897e4f12aa1f9a134dd78e2157cf
anniesk <annistaaax@gmail.com>
2023-03-31 15:40:32 +0300

    get bin counts for suomi24 data

---
+++ miscellaneous_stuff/hist.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+
+from collections import Counter, defaultdict
+from argparse import ArgumentParser
+
+
+def argparser():
+    ap = ArgumentParser()
+    ap.add_argument('-i', '--interval', type=float, default=0.1)
+    ap.add_argument('tsv')
+    return ap
+
+
+def main(argv):
+    args = argparser().parse_args(argv[1:])
+    
+    counts = defaultdict(Counter)
+    with open(args.tsv) as f:
+        header = next(f)
+        labels = header.rstrip('\n').split('\t')[1:]    # 1st col is ID
+        for line in f:
+            values = line.rstrip('\n').split('\t')[1:]
+            for label, value in zip(labels, values):
+                bin_ = int(float(value)/args.interval)
+                counts[label][bin_] += 1
+
+    bins = sorted(set([b for lc in counts.values() for b in lc]))
+
+    itv = args.interval
+    bin_range = { b: f'{itv*b:.1f}-{itv*(b+1):.1f}' for b in bins }
+    print('\t'.join(['label'] + [bin_range[b] for b in bins]))
+    
+    for label in counts:
+        print('\t'.join([label] + [str(counts[label][b]) for b in bins]))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
