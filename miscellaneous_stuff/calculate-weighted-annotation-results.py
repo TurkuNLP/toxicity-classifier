ad9aab0ebbe101e9f3033c85436805b2445130ac
anniesk <annistaaax@gmail.com>
2023-04-13 13:44:26 +0300

    finished getting final numbers

--- miscellaneous_stuff/calculate-weighted-annotation-results.py
+++ miscellaneous_stuff/calculate-weighted-annotation-results.py
@@ -19,3 +19,127 @@
 #     -> then save the numbers somewhere or read the bin weights and already do some calculations :D 
 #     all in all: does not sound too bad? unless I run into some random issues of course.
 
+# IF i want everything at once I would have to do a double loop but doable actually?
+# cd miscellaneous_stuff/new_batches/sample-01-LABEL
+# echo -e "TP \t TN \t FP \t FN" >> ../weighted-results.tsv | for f in * ; do python3 ../../calculate-weighted-annotation-results.py ../../pictures_and_other_files/binned_amounts.tsv $f insult >> ../weighted-results.tsv; done
+# append to file
+
+
+import sys # is this used?
+
+from argparse import ArgumentParser
+
+
+def argparser():
+    ap = ArgumentParser()
+    ap.add_argument('weights', help='binned_amounts.tsv')
+    ap.add_argument('annotations', help='tsv')
+    ap.add_argument('label', help='e.g. "obscene"')
+    return ap
+
+
+# final calculations, read the weighted-results.tsv file and sum up each column and then do the regular prec, rec, f1 calculations
+# either use the formulas and do it "manually" or look at if the ready made stuff can use these numbers
+
+# https://towardsdatascience.com/evaluating-multi-label-classifiers-a31be83da6ea here the micro-avg for precision really helped me understand how to do this
+# just sum like I mentioned and do that
+
+def calculate():
+    # read file
+    with open("weighted-results.tsv") as f:
+        data = f.readlines()
+        data = data[1:]
+        for i in range(len(data)):
+            data[i] = data[i].replace("\n", "")
+            data[i] = data[i].split("\t")
+
+    # loop and sum columns to a new list
+    int_data = []
+    for i in range(len(data)):
+        lista = [eval(i) for i in data[i]]
+        int_data.append(lista)
+    summed = [sum(i) for i in zip(*int_data)]
+    TP = summed[0]
+    TN = summed[1]
+    FP = summed[2]
+    FN = summed[3]
+
+    # these will be MICRO
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = 2 * TP / (2 * TP + FP + FN)
+
+    return precision, recall, f1
+
+    # these give biiig numbers... I probably should adjust the weight numbers
+    #  by dividing them by the number of comments in the file where the sampling was made because these raw numbers make it impossible
+    # results are still??
+
+
+def main(argv):
+    prec, rec, f1 = calculate()
+    print("precision:", prec)
+    print("recall:", rec)
+    print("f1", f1)
+
+
+    # args = argparser().parse_args(argv[1:])
+
+    # # this has the data from one bin file of the specified label
+    # # format is [id, label, text, probability]
+    # with open(args.annotations) as f:
+    #     data = f.readlines()
+    #     data = data[1:]
+    #     for i in range(len(data)):
+    #         data[i] = data[i].replace("\n", "")
+    #         data[i] = data[i].split("\t")
+    
+    # # after this weights has [label, bin1, bin2, etc. ]
+    # # so when doing this I just need to find the correct row (index) and then loop the rest of the "columns" in that
+    # with open(args.weights) as f:
+    #     weights = f.readlines()
+    #     weights = weights[1:]
+    #     for i in range(len(weights)):
+    #         weights[i] = weights[i].replace("\n", "")
+    #         weights[i] = weights[i].split("\t")
+
+
+    # # here is where the magic happens lol
+    # TP = 0
+    # TN = 0
+    # FP = 0
+    # FN = 0
+    # for i in range(len(data)):
+    #     if "not" in data[i][1] and float(data[i][3]) >= 0.5:
+    #         FP += 1
+    #     elif "not" in data[i][1] and float(data[i][3]) < 0.5:
+    #         TN += 1
+    #     elif not "not" in data[i][1] and float(data[i][3]) < 0.5:
+    #         FN += 1
+    #     elif not "not" in data[i][1] and float(data[i][3]) >= 0.5:
+    #         TP += 1
+
+    # # get index of the current label that is used
+    # for i in range(len(weights)):
+    #     if args.label == weights[i][0]:
+    #         index = i
+    #         break
+
+    # # multiply the counts with the weights
+    # for i in range(len(weights[index])):
+    #     if i == 0:
+    #         continue
+    #     else:
+    #         weighted_TP = TP * int(weights[index][i])
+    #         weighted_TN = TN * int(weights[index][i])
+    #         weighted_FP = FP * int(weights[index][i])
+    #         weighted_FN = FN * int(weights[index][i])
+
+    # # here print the stuff lol
+    # print(TP,"\t", TN, "\t", FP,"\t", FN) # unweighted
+    # #print(weighted_TP, "\t", weighted_TN, "\t", weighted_FP, "\t", weighted_FN)
+    # # by default the results tsv looks funny because it is split like that due to the bins anyway
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
